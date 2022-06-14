72e35d4973e0f991853104687fe3666c8f96aa04
lossdev <bmwilson.dev@gmail.com>
2022-06-14 17:05:03 -0600

    updated example funcs

--- new_example_test.go
+++ new_example_test.go
@@ -9,23 +9,29 @@ import (
 )
 
 func ExampleWebsocket_ServerSocket() {
+	// this code should be placed in an HTTP handler function
+	// i.e func foo(w http.ResponseWriter, r *http.Request) { ... }
 	ws := websockit.NewWebsocket()
-	opts := []websockit.WebsocketOption{
-		websockit.WithHandshakeTimeout(5 * time.Second),
-		websockit.WithProxy(http.ProxyFromEnvironment),
+	opts := []websockit.WebsocketServerOption{
+		websockit.ServerWithHandshakeTimeout(5 * time.Second),
 	}
-	if err := ws.ServerSocket("http://localhost:8000", nil, opts...); err != nil {
+
+	// this is just so the snippet compiles - you'd already have `w` and `req` in scope and defined here
+	var w http.ResponseWriter
+	var req *http.Request
+
+	if err := ws.ServerSocket(w, req, nil, opts...); err != nil {
 		log.Println(err)
 	}
 }
 
 func ExampleWebsocket_ClientSocket() {
 	ws := websockit.NewWebsocket()
-	opts := []websockit.WebsocketOption{
-		websockit.WithHandshakeTimeout(5 * time.Second),
-		websockit.WithProxy(http.ProxyFromEnvironment),
+	opts := []websockit.WebsocketClientOption{
+		websockit.ClientWithHandshakeTimeout(5 * time.Second),
+		websockit.ClientWithProxy(http.ProxyFromEnvironment),
 	}
-	if err := ws.ClientSocket("http://localhost:8000", nil, opts...); err != nil {
+	if err := ws.ClientSocket("wss://ws.postman-echo.com/raw", nil, opts...); err != nil {
 		log.Println(err)
 	}
 }
