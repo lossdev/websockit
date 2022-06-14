c174b9902c43f72526caf772f15efb895afc88a9
lossdev <bmwilson.dev@gmail.com>
2022-06-13 23:36:14 -0600

    add docs file

---
+++ doc.go
@@ -0,0 +1,8 @@
+// package websockit aims to provide a toolkit to quickly and easily get started with a websocket server, client, or both
+// with built in connection and error handling.
+
+// Traditionally, implementing either end of a websocket pair can be a tricky task that requires a lot of boilerplate and
+// docs reading. websockit aims to try and eliminate the most downtime from this process as possible by providing a lot of
+// the generic framework around websockets.
+
+package websockit
